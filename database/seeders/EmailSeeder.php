03ed0364db17ea32694115a2f1fa44bf070e1a1f
Tijn <tijnvanderweijden@gmail.com>
2021-06-15 16:27:06 +0200

    extra test emails toegevoegd

--- database/seeders/EmailSeeder.php
+++ database/seeders/EmailSeeder.php
@@ -18,5 +18,25 @@ class EmailSeeder extends Seeder
             'mailgroep-id' => 1,
             'email' => "testemail@invuller.com"
         ]);
+
+        DB::table('emailadressen')->insert([
+            'mailgroep-id' => 1,
+            'email' => "testemail2@invuller.com"
+        ]);
+
+        DB::table('emailadressen')->insert([
+            'mailgroep-id' => 1,
+            'email' => "testemail3@invuller.com"
+        ]);
+
+        DB::table('emailadressen')->insert([
+            'mailgroep-id' => 1,
+            'email' => "testemail4@invuller.com"
+        ]);
+
+        DB::table('emailadressen')->insert([
+            'mailgroep-id' => 1,
+            'email' => "testemail5@invuller.com"
+        ]);
     }
 }
